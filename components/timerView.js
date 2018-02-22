a03e7c8719e61a8e2800dcc9c9183fb433eb865e
Gabriel Trujillo C <joaqt23@gmail.com>
2018-02-22 04:26:25 -0500

    02-DevTools

--- components/timerView.js
+++ components/timerView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {observer} from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
 
 @observer
 export default class TimerView extends React.Component {
@@ -9,9 +10,17 @@ export default class TimerView extends React.Component {
 
     render() {
         return (
-            <button onClick={this.onReset}>
-                Seconds passed: {this.props.appState.elapsedTime}
-            </button>
+            <React.Fragment>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <DevTools position={{top: 20}} highlightTimeout={5000}/>
+                <button onClick={this.onReset}>
+                    Seconds passed: {this.props.appState.elapsedTime}
+                </button>
+            </React.Fragment>
         );
     }
 };
